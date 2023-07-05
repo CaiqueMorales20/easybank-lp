d39d456cdce612e8fb8f8ce726ff8e798363d734
Caique <caiquemorales20@gmail.com>
2023-07-04 23:39:24 -0300

    :heavy_minus_sign: build: styled-components

---
+++ src/global/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+// Imports
+import { createGlobalStyle } from "styled-components";
+
+// Styled Components
+export const GlobalStyle = createGlobalStyle`
+  /* ==================== HTML Reseting ==================== */
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Styles
+import { GlobalStyle } from "./global/GlobalStyle.tsx";
+
 // App
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
+		<GlobalStyle />
 		<App />
 	</React.StrictMode>
 );
