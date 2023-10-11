99830e6f402c826525c3292937a807ca428d2119
Caique Morales <caiquemorales20@gmail.com>
2023-10-10 23:07:32 -0300

    :construction: About

---
+++ src/app/(About)/index.tsx
@@ -0,0 +1,13 @@
+// Functional Component
+export default function About() {
+  // Rendering
+  return (
+    <section>
+      <h1>Why choose Easybank?</h1>
+      <p>
+        We leverage Open Banking to turn your bank account into financial hub.
+        <br /> Control your finances like never before
+      </p>
+    </section>
+  )
+}
