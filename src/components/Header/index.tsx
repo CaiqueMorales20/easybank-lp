47904997fcbe0e1c16f7fa13983ad819152cab68
Caique Morales <caiquemorales20@gmail.com>
2023-10-15 21:33:29 -0300

    :sparkles: feat: Mobile Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
+// Client side
+'use client'
+
 // Imported Components
 import Image from 'next/image'
+import { useState } from 'react'
 
 // Functional Component
 export default function Header() {
+  // Variables
+  const [menuOpened, setMenuOpened] = useState(false)
+
   // Rendering
   return (
     <header className="shadow-sm absolute z-40 w-full bg-white">
-      <nav className="w-[90vw] max-w-[1880px] bg-white z-20 mx-auto flex justify-between  md:grid md:grid-cols-3 h-24 items-center ">
+      <nav className="w-[90vw] max-w-[1880px] bg-white  z-20 mx-auto flex justify-between  md:grid md:grid-cols-3 h-24 items-center ">
         {/* Logo */}
         <Image
           src="/icon/logo.svg"
@@ -15,7 +22,10 @@ export default function Header() {
           height={20}
         />
         {/* Menu */}
-        <ul className="hidden md:flex items-center h-full justify-self-center gap-10">
+        <ul
+          data-opened={menuOpened}
+          className="absolute opacity-0 data-[opened=true]:opacity-100 duration-300 md:opacity-100 data-[opened=true]:flex bg-white z-[9999] w-[90vw] md:w-full shadow-md md:shadow-none gap-6 py-10 md:py-0 md:static flex flex-col top-28 md:flex-row items-center md:h-full justify-self-center md:gap-10"
+        >
           <li className="h-full flex items-center hover:border-b-secondary border-b-[4px] border-b-transparent duration-300 group">
             <a
               href="#"
@@ -63,11 +73,12 @@ export default function Header() {
         </a>
         {/* Hamburguer Button */}
         <Image
+          onClick={() => setMenuOpened(!menuOpened)}
           className="md:hidden"
-          src="/icon/hamburger.svg"
+          src={menuOpened ? '/icon/close.svg' : '/icon/hamburger.svg'}
           alt="Abrir menu"
-          width={24}
-          height={11}
+          width={menuOpened ? 17 : 24}
+          height={menuOpened ? 17 : 11}
         />
       </nav>
     </header>
