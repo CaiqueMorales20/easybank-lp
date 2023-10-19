3fb6ceeacb78aecfc8c29df3f9bf4df390992fc3
Caique Morales <caiquemorales20@gmail.com>
2023-10-18 23:33:04 -0300

    :sparkles: feat: Footer socials

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,13 +7,45 @@ export default function Footer() {
     <footer className="bg-primary py-[48px]">
       <div className="container flex gap-[131px]">
         {/* Logo */}
-        <div>
+        <div className="flex flex-col justify-between">
           <Image
             src="/icon/logo-footer.svg"
             alt="Logo do footer"
             width={139}
             height={20}
           />
+          <div className="flex gap-[13.33px]">
+            <Image
+              src="/icon/facebook.svg"
+              alt="Open in facebook"
+              width={20}
+              height={20}
+            />
+            <Image
+              src="/icon/youtube.svg"
+              alt="Open in Youtube"
+              width={20}
+              height={20}
+            />
+            <Image
+              src="/icon/twitter.svg"
+              alt="Open in Twitter"
+              width={16.26}
+              height={20}
+            />
+            <Image
+              src="/icon/pinterest.svg"
+              alt="Open in Pinterest"
+              width={20}
+              height={20}
+            />
+            <Image
+              src="/icon/instagram.svg"
+              alt="Open in Instagram"
+              width={20}
+              height={20}
+            />
+          </div>
         </div>
         {/* Links */}
         <div className="grid grid-cols-2 gap-[30px]">
